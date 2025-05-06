29b54fd7112c79e07ac3c5fac6a6fd71464c00d1
donghyunlee <azazs0000@naver.com>
2025-05-06 20:53:06 +0900

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <router-view />
+  테트입니다.
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
 });
 </script>
